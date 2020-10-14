025c8b4292cfa0adf72d77632bf86d9e74054afe
Zengo-Master <8435740@gmail.com>
2020-10-14 23:05:22 +0900

    js改行

--- app/javascript/win.js
+++ app/javascript/win.js
@@ -133,120 +133,120 @@ var json_data = {"_1":{
 
 // 初期処理
 function init(){
-var trs = document.getElementById("winorlose_table").getElementsByTagName('tr');
-// 1行目はヘッダーだから飛ばして
-for(var itr = 1; itr < trs.length; itr++){
-var win = 0;
-var lose = 0;
-// テーブルの行毎に処理
-var tds = trs[itr].getElementsByTagName('td');
-for(var itd = 0; itd < tds.length; itd++){
-if(tds[itd].id.indexOf('op_') != -1){ // idにop_を含む（勝敗記入のtd）
-// 勝ち負け入力<td>タグの場合
-var arr = tds[itd].id.split('_'); // op_数字_数字 → [op, 数字, 数字]
-if(arr[1] == arr[2]){
-// 対戦が同じ場合
-tds[itd].classList.add('right_down_border'); // 縦横同値マスにクラスを指定
-}
-else {
-// 勝ち数を加算
-if(tds[itd].innerHTML == '○'){
-win ++;
-}
-// 負け数を加算
-else if(tds[itd].innerHTML == '●'){
-lose ++;
-}
-// 対戦が違う場合、イベントを登録
-// onclick="winorlose_click(this);" と同じ内容
-tds[itd].addEventListener('click', {name: this, handleEvent: winorlose_click});
-if(json_data['_'+ arr[1]]['_'+ arr[2]]["WinOrLose"] == 1){
-  tds[itd].innerHTML = "○";
-}
-else if(json_data['_'+ arr[1]]['_'+ arr[2]]["WinOrLose"] == -1) {
-  tds[itd].innerHTML = "●";
-}
-}
-}
-else if(tds[itd].id.indexOf('win_') != -1) {
-// 勝ち表示
-tds[itd].innerHTML = win;
-}
-else if(tds[itd].id.indexOf('lose_') != -1) {
-// 負け表示
-tds[itd].innerHTML = lose;
-}
-}
-}
+  var trs = document.getElementById("winorlose_table").getElementsByTagName('tr');
+  // 1行目はヘッダーだから飛ばして
+  for(var itr = 1; itr < trs.length; itr++){
+  var win = 0;
+  var lose = 0;
+      // テーブルの行毎に処理
+      var tds = trs[itr].getElementsByTagName('td');
+      for(var itd = 0; itd < tds.length; itd++){
+          if(tds[itd].id.indexOf('op_') != -1){ // idにop_を含む（勝敗記入のtd）
+              // 勝ち負け入力<td>タグの場合
+              var arr = tds[itd].id.split('_'); // op_数字_数字 → [op, 数字, 数字]
+              if(arr[1] == arr[2]){
+                  // 対戦が同じ場合
+                  tds[itd].classList.add('right_down_border'); // 縦横同値マスにクラスを指定
+              }
+              else {
+                  // 勝ち数を加算
+                  if(tds[itd].innerHTML == '○'){
+                    win ++;
+                  }
+                  // 負け数を加算
+                  else if(tds[itd].innerHTML == '●'){
+                    lose ++;
+                  }
+                  // 対戦が違う場合、イベントを登録
+                  // onclick="winorlose_click(this);" と同じ内容
+                  tds[itd].addEventListener('click', {name: this, handleEvent: winorlose_click});
+                  if(json_data['_'+ arr[1]]['_'+ arr[2]]["WinOrLose"] == 1){
+                      tds[itd].innerHTML = "○";
+                  }
+                  else if(json_data['_'+ arr[1]]['_'+ arr[2]]["WinOrLose"] == -1) {
+                      tds[itd].innerHTML = "●";
+                  }
+              }
+          }
+          else if(tds[itd].id.indexOf('win_') != -1) {
+            // 勝ち表示
+            tds[itd].innerHTML = win;
+          }
+          else if(tds[itd].id.indexOf('lose_') != -1) {
+            // 負け表示
+            tds[itd].innerHTML = lose;
+          }
+      }
+  }
 }
 
 // クリックしたときの処理
 var send_winorlose = false;
 var XHR;
 function winorlose_click(el){
-if(send_winorlose) return;    // 送信中は実行できない
-send_winorlose = true;        // 処理中とする
+    if(send_winorlose) return;    // 送信中は実行できない
+    send_winorlose = true;        // 処理中とする
 
-// el にはどこでクリックされたかの情報が入っている
-var arr = el.target.id.split('_');                 // idを'_'で分ける .targetで親要素イベントも発火
-var oppid = arr[0] + '_' + arr[2] + '_'+ arr[1];   // 反対側のid
-// 勝敗の記入と取消
-if(el.target.innerHTML == ''){                     // 勝敗を記入
-el.target.innerHTML = '○';                       // 勝ち
-document.getElementById(oppid).innerHTML = '●';  // 負け 
-json_data['_'+ arr[1]]['_'+ arr[2]]["WinOrLose"] = 1;           //jsonのデータ自体を更新
-json_data['_'+ arr[2]]['_'+ arr[1]]["WinOrLose"] = -1;          //jsonのデータ自体を更新
-}
-else if(el.target.innerHTML == '○'){               // 勝敗を取消
-el.target.innerHTML = '';                        // 空白
-document.getElementById(oppid).innerHTML = '';   // 空白
-json_data['_'+ arr[1]]['_'+ arr[2]]["WinOrLose"] = 0;           //jsonのデータ自体を更新
-json_data['_'+ arr[2]]['_'+ arr[1]]["WinOrLose"] = 0;           //jsonのデータ自体を更新
-}
+    // el にはどこでクリックされたかの情報が入っている
+    var arr = el.target.id.split('_');                 // idを'_'で分ける .targetで親要素イベントも発火
+    var oppid = arr[0] + '_' + arr[2] + '_'+ arr[1];   // 反対側のid
+    // 勝敗の記入と取消
+    if(el.target.innerHTML == ''){                     // 勝敗を記入
+      el.target.innerHTML = '○';                       // 勝ち
+      document.getElementById(oppid).innerHTML = '●';  // 負け 
+      json_data['_'+ arr[1]]['_'+ arr[2]]["WinOrLose"] = 1;           //jsonのデータ自体を更新
+      json_data['_'+ arr[2]]['_'+ arr[1]]["WinOrLose"] = -1;          //jsonのデータ自体を更新
+    }
+    else if(el.target.innerHTML == '○'){               // 勝敗を取消
+      el.target.innerHTML = '';                        // 空白
+      document.getElementById(oppid).innerHTML = '';   // 空白
+      json_data['_'+ arr[1]]['_'+ arr[2]]["WinOrLose"] = 0;           //jsonのデータ自体を更新
+      json_data['_'+ arr[2]]['_'+ arr[1]]["WinOrLose"] = 0;           //jsonのデータ自体を更新
+    }
 
-// 勝ち数と負け数の計算
-var trs = document.getElementById("winorlose_table").getElementsByTagName('tr');
-var win = 0;
-var lose = 0;
-// 1行目はヘッダーだから飛ばして
-for(var itr = 1; itr < trs.length; itr++){
-// テーブルの行毎に処理
-win = 0;
-lose = 0;
-var tds = trs[itr].getElementsByTagName('td');
-for(var itd = 0; itd < tds.length; itd++){
-if(tds[itd].id.indexOf('op_') != -1){
-// 勝ち負け数カウント
-if(tds[itd].innerHTML == '○'){
-win ++;
-} else if(tds[itd].innerHTML == '●'){
-lose ++;
-}
-} else if(tds[itd].id.indexOf('win_') != -1){
-// 勝ち表示
-tds[itd].innerHTML = win;
-} else if(tds[itd].id.indexOf('lose_') != -1){
-// 負け表示
-tds[itd].innerHTML = lose;
-}
-}
-}
+    // 勝ち数と負け数の計算
+    var trs = document.getElementById("winorlose_table").getElementsByTagName('tr');
+    var win = 0;
+    var lose = 0;
+    // 1行目はヘッダーだから飛ばして
+    for(var itr = 1; itr < trs.length; itr++){
+        // テーブルの行毎に処理
+        win = 0;
+        lose = 0;
+        var tds = trs[itr].getElementsByTagName('td');
+        for(var itd = 0; itd < tds.length; itd++){
+            if(tds[itd].id.indexOf('op_') != -1){
+                // 勝ち負け数カウント
+                if(tds[itd].innerHTML == '○'){
+                    win ++;
+                } else if(tds[itd].innerHTML == '●'){
+                    lose ++;
+                }
+            } else if(tds[itd].id.indexOf('win_') != -1){
+                // 勝ち表示
+                tds[itd].innerHTML = win;
+            } else if(tds[itd].id.indexOf('lose_') != -1){
+                // 負け表示
+                tds[itd].innerHTML = lose;
+            }
+        }
+    }
 
-//※ データの送信は処理の最後
-//※ 非同期でも結果が返ってくるまでクリックを無視するように変更
-let resultId = el.target.getAttribute("data-id");
-if(!XHR){
-XHR = new XMLHttpRequest();
-XHR.responseType = "json";
-XHR.onload = function() {
-send_winorlose = false;    // 送信完了
-};
-XHR.onerror = function() {
-send_winorlose = false;    // 送信完了
-};
-}
-XHR.open("GET", `/results/${resultId}`, true);
-XHR.send();
+    //※ データの送信は処理の最後
+    //※ 非同期でも結果が返ってくるまでクリックを無視するように変更
+    let resultId = el.target.getAttribute("data-id");
+    if(!XHR){
+        XHR = new XMLHttpRequest();
+        XHR.responseType = "json";
+        XHR.onload = function() {
+            send_winorlose = false;    // 送信完了
+        };
+        XHR.onerror = function() {
+            send_winorlose = false;    // 送信完了
+        };
+    }
+    XHR.open("GET", `/results/${resultId}`, true);
+    XHR.send();
 }
 
 window.addEventListener("load", init);
