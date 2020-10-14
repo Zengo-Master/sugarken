a4c7d21487916bfc01ac6a33385acdca0d15664e
Zengo-Master <8435740@gmail.com>
2020-10-15 00:04:32 +0900

    棋譜準備

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -1,7 +1,44 @@
 class ResultsController < ApplicationController
+  before_action :set_item, only: [:edit, :update, :show, :destroy]
 
   def index
     @members = Member.all
+  end
+
+  def new
+    @kifu = Result.new
+  end
+
+  def create
+    @kifu = Result.create(result_params)
+    if @kifu.valid?
+      @kifu.save
+      redirect_to root_path
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @result.update(result_params)
+      redirect_to result_path(@result.id)
+    else
+      render 'edit'
+    end
+  end
+
+  def show
+  end
+
+  def destroy
+    if @kifu.destroy
+      redirect_to root_path
+    else
+      redirect_to result_path(@result.id)
+    end
   end
 
   def win_or_lose
@@ -15,6 +52,16 @@ class ResultsController < ApplicationController
       result_win.update(result: "○")
       result_lose.update(result: "●")
     end
+  end
+
+  private
+
+  def result_params
+    params.require(:result).permit(:kifu)
+  end
+
+  def set_result
+    @result = Result.find(params[:id])
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   root to: 'results#index'
+  # resources :results
   get 'results/:id', to: 'results#win_or_lose'
 end
